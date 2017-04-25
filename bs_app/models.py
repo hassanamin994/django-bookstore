1a63ef55ae4ea26f6bd377730ed40fbf807842d1
Hassan Amin <hassanmamin994@gmail.com>
2017-04-25 03:50:12 +0200

    Add bs_app/main bookstore  application

---
+++ bs_app/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100,null=False)
+
+class Author(models.Model):
+
+    name = models.CharField(max_length=100,null=False)
+    bio = models.TextField()
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=100,null=False)
+    description = models.TextField(null=False)
+    authors = models.ManyToManyField('Author')
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE )
+    authors = models.ManyToManyField('Author')
+    categories = models.ManyToManyField('Category')
+    books = models.ManyToManyField('Book',through='Rate')
+
+#Synchronizing User model with Profile model
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Rate(models.Model):
+
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE )
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    STATES = [('read','Read'),('wish','Wish'),('none','None')]
+    state = models.CharField(max_length=10,choices=STATES)
+    RATES = [(1,"1"),(2,"2"),(3,"3"),(4,"4"),(5,"5"),(6,"6"),(7,"7"),(8,"8"),(9,"9"),(10,"10")]
+    rate = models.CharField(max_length=2,choices=RATES)
