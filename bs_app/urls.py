2e680a6a6b3573a0cb1f83e8d0b622a2585e7561
Hassan Amin <hassanmamin994@gmail.com>
2017-04-25 23:50:08 +0200

    Add Categories details view

--- bs_app/urls.py
+++ bs_app/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^home/', home),
     url(r'^books/(?P<pk>[0-9]+)$', books.BookDetail.as_view(), name="book_detail"),
     url(r'^books/', books.BookList.as_view(),name="book_list"),
+
+    url(r'^categories/(?P<category_id>[0-9]+)', categories.category_detail,name="category_detail"),
     url(r'^categories/', categories.category_list,name="category_list"),
     url(r'^admin/', admin.site.urls),
 
--- bs_app/views/categories.py
+++ bs_app/views/categories.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_list_or_404,get_object_or_404
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
-from bs_app.models import Category
+from bs_app.models import Category, Book
 from django.views.generic import ListView, DetailView
 # Create your views here.
 
@@ -17,3 +17,21 @@ def category_list(request):
     categories = Category.objects.all()
     my_categories = Category.objects.filter(profile=request.user.id)
     return render(request,'bs_app/category_list.html', {'categories':categories,'my_categories':my_categories})
+
+def category_detail(request, category_id):
+    print(category_id)
+    category = Category.objects.get(pk=category_id)
+    books = Book.objects.filter(category=category_id)
+
+    return render(request,'bs_app/category_detail.html',{'category':category,'books':books})
+
+
+
+def category_subscribe(request):
+    print('subscribe')
+    # pass
+
+
+def category_unsubscribe(request):
+    print('unsubscribe')
+    # pass
