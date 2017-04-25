7af06d2cf0b6b4a335669413057343628b871592
Hassan Amin <hassanmamin994@gmail.com>
2017-04-25 17:53:48 +0200

    Change passwordfields in login/registeration widgets

--- bs_authentication/forms/registeration_form.py
+++ bs_authentication/forms/registeration_form.py
@@ -3,8 +3,8 @@ from django import forms
 class RegisterationForm(forms.Form):
 
     username = forms.CharField(required=True)
-    password = forms.CharField(required=True)
-    confirm_password = forms.CharField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput(),required=True)
+    confirm_password = forms.CharField(widget=forms.PasswordInput(),required=True)
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
