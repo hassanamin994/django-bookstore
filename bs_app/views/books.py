df14e3f110e98aed858ef2876e0937495b9453af
Hassan Amin <hassanmamin994@gmail.com>
2017-04-27 00:22:34 +0200

    Book: show average rating of the book for the user

--- bs_app/views/books.py
+++ bs_app/views/books.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from bs_app.models import Book, Rate
 from django.views.generic import ListView, DetailView
+from django.db.models import Avg
 # Create your views here.
 
 # @require_http_methods(['GET'])
@@ -24,7 +25,8 @@ def book_detail(request, book_id):
     if len(rate) == 1 :
         rate = rate[0]
     rates_list = map(str, range(1,10))
-    return render(request, 'bs_app/book_detail.html',{'object': book, 'properties': rate, 'rate_list':rates_list })
+    average_rate =book.rate_set.aggregate(Avg('rate'))['rate__avg']
+    return render(request, 'bs_app/book_detail.html',{'object': book, 'properties': rate, 'rate_list':rates_list, 'average_rating': average_rate})
 
 def book_rate(request, book_id, new_rate):
     rate = get_rate(request.user.profile, book_id)
