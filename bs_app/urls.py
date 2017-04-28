1b5b0d54287411d0ff866e9a925b4055b96c5ef1
Hassan Amin <hassanmamin994@gmail.com>
2017-04-28 14:49:39 +0200

    Add author list/detail view

--- bs_app/urls.py
+++ bs_app/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from .views.home import home
-from .views import books
-from .views import categories
+from .views import books, categories, authors
+
 app_name="app"
 urlpatterns = [
     url(r'^home/', home),
@@ -11,6 +11,11 @@ urlpatterns = [
     url(r'^books/(?P<book_id>[0-9]+)/read$', books.book_read, name="book_detail"),
     url(r'^books/(?P<book_id>[0-9]+)$', books.book_detail, name="book_detail"),
     url(r'^books/', books.book_list,name="book_list"),
+
+    url(r'^authors/(?P<author_id>[0-9]+)/unsubscribe', authors.author_unsubscribe,name="category_detail"),
+    url(r'^authors/(?P<author_id>[0-9]+)/subscribe', authors.author_subscribe,name="category_detail"),
+    url(r'^authors/(?P<author_id>[0-9]+)', authors.author_detail,name="category_detail"),
+    url(r'^authors/', authors.author_list,name="category_detail"),
 
     url(r'^categories/(?P<category_id>[0-9]+)/unsubscribe', categories.category_unsubscribe,name="category_detail"),
     url(r'^categories/(?P<category_id>[0-9]+)/subscribe', categories.category_subscribe,name="category_detail"),
---
+++ bs_app/views/authors.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,get_list_or_404,get_object_or_404, redirect
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from bs_app.models import Book, Rate, Author
+from django.db.models import Avg
+
+
+def author_list(request):
+    authors = Author.objects.all()
+    my_authors = Author.objects.filter(profile=request.user.id)
+    return render(request,'bs_app/author_list.html', {'authors':authors,'my_authors':my_authors})
+
+def author_detail(request, author_id):
+    author = Author.objects.get(pk=author_id)
+    books = Book.objects.filter(authors=author_id)
+    return render(request,'bs_app/author_detail.html',{'author':author,'books':books})
+    pass
+
+def author_subscribe(request, author_id):
+    pass
+
+def author_unsubscribe(request, author_id):
+    pass
