511f63be68f8ed40893e32c8a6c9f51812181a4d
Hassan Amin <hassanmamin994@gmail.com>
2017-04-28 15:46:38 +0200

    Add home page, shows latest books in user's categories, authors and general latest 10 added books

--- bs_app/views/home.py
+++ bs_app/views/home.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from bs_app.models import Book, Rate, Category
 # Create your views here.
 
 def home(request):
-    pass
+    my_categories = []
+    my_authors = []
 
+    # getting list of user's categories ids
+    for category in request.user.profile.categories.all():
+        my_categories.append(category.id)
+    #getting list for user's authors ids
+    for author in request.user.profile.authors.all():
+        my_authors.append(author.id)
 
-
-
-    
+    categories_books = Book.objects.filter(category__in=my_categories)
+    authors_books = Book.objects.filter(authors__in=my_authors)
+    latest_books =  Book.objects.all().order_by('-id')[:10]
+    return render(request, 'bs_app/home.html',{'categories_books': categories_books, 'authors_books' : authors_books ,'latest_books': latest_books})
