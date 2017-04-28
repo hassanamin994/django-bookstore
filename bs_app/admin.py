1ab9942ed5cac5305c3c3283f710741b47c4a2b0
Hassan Amin <hassanmamin994@gmail.com>
2017-04-28 23:40:38 +0200

    Notifications: add notification for for categories

--- bs_app/admin.py
+++ bs_app/admin.py
@@ -7,9 +7,21 @@ class BookForm(forms.ModelForm):
     def save(self, commit=True):
         print("saving ")
         instance = super(BookForm, self).save(commit=False)
-        if commit:
-            instance.save()
-        return instance
+
+
+        instance.save()
+        notification_category_body = "A new book was added in " + instance.category.name
+        notification_category = models.Notification.objects.create(body=notification_category_body)
+        for profile in instance.category.profile_set.all():
+            profile.notifications.add(notification_category)
+        # print('the count is ' + str(instance.authors.all().count()))
+        # for author in instance.authors.all():
+        #     notification_author_body = author.name + " Has published a new book"
+        #     notification_author = models.Notification.objects.create(body=notification_author_body)
+        #     for profile in author.profile_set.all():
+        #         profile.notifications.add(notification_author)
+        #         profile.notifications.save()
+        # return instance
     class Meta:
         model=models.Book
         fields = "__all__"
