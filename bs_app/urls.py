cbc63082494657cb74535b77eb6c4ac3b3593529
Hassan Amin <hassanmamin994@gmail.com>
2017-04-28 18:32:39 +0200

    Add Search by book/author

--- bs_app/urls.py
+++ bs_app/urls.py
@@ -1,24 +1,27 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from .views.home import home
-from .views import books, categories, authors
+from .views import books, categories, authors, search
 
 app_name="app"
 urlpatterns = [
     url(r'^home/', home),
-    url(r'^books/(?P<book_id>[0-9]+)/rate/(?P<new_rate>[0-9]+)$', books.book_rate, name="book_detail"),
-    url(r'^books/(?P<book_id>[0-9]+)/wish$', books.book_wish, name="book_detail"),
-    url(r'^books/(?P<book_id>[0-9]+)/read$', books.book_read, name="book_detail"),
+    url(r'^books/(?P<book_id>[0-9]+)/rate/(?P<new_rate>[0-9]+)$', books.book_rate, name="book_rate"),
+    url(r'^books/(?P<book_id>[0-9]+)/wish$', books.book_wish, name="book_wish"),
+    url(r'^books/(?P<book_id>[0-9]+)/read$', books.book_read, name="book_read"),
     url(r'^books/(?P<book_id>[0-9]+)$', books.book_detail, name="book_detail"),
     url(r'^books/', books.book_list,name="book_list"),
 
-    url(r'^authors/(?P<author_id>[0-9]+)/unsubscribe', authors.author_unsubscribe,name="category_detail"),
-    url(r'^authors/(?P<author_id>[0-9]+)/subscribe', authors.author_subscribe,name="category_detail"),
-    url(r'^authors/(?P<author_id>[0-9]+)', authors.author_detail,name="category_detail"),
+    url(r'^search/book/(?P<query>[a-zA-Z0-9]+)$', search.book_search, name="book_search"),
+    url(r'^search/author/(?P<query>[a-zA-Z0-9]+)$', search.author_search, name="author_search"),
+
+    url(r'^authors/(?P<author_id>[0-9]+)/unsubscribe', authors.author_unsubscribe,name="author_unsubscribe"),
+    url(r'^authors/(?P<author_id>[0-9]+)/subscribe', authors.author_subscribe,name="author_subscribe"),
+    url(r'^authors/(?P<author_id>[0-9]+)', authors.author_detail,name="author_detail"),
     url(r'^authors/', authors.author_list,name="category_detail"),
 
-    url(r'^categories/(?P<category_id>[0-9]+)/unsubscribe', categories.category_unsubscribe,name="category_detail"),
-    url(r'^categories/(?P<category_id>[0-9]+)/subscribe', categories.category_subscribe,name="category_detail"),
+    url(r'^categories/(?P<category_id>[0-9]+)/unsubscribe', categories.category_unsubscribe,name="category_unsubscribe"),
+    url(r'^categories/(?P<category_id>[0-9]+)/subscribe', categories.category_subscribe,name="category_subscribe"),
     url(r'^categories/(?P<category_id>[0-9]+)', categories.category_detail,name="category_detail"),
     url(r'^categories/', categories.category_list,name="category_list"),
     url(r'^admin/', admin.site.urls),
---
+++ bs_app/views/search.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render,get_list_or_404,get_object_or_404, redirect
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from bs_app.models import Book, Rate, Author
+from django.views.generic import ListView, DetailView
+from django.db.models import Avg
+# Create your views here.
+
+# @require_http_methods(['GET'])
+# def book_list(request):
+#     books = get_list_or_404(Book)
+#     for book in books:
+#         print( book.title)
+#
+#     return HttpResponse('books list')
+
+def book_search(request, query):
+    books = Book.objects.filter(title__contains=query)
+
+    return render(request, 'bs_app/search.html',{'search_type':'book', 'books': books })
+
+def author_search(request, query):
+    authors = Author.objects.filter(name__contains=query)
+    my_authors = Author.objects.filter(profile=request.user.id)
+    return render(request,'bs_app/search.html', {'search_type':'author', 'authors':authors,'my_authors':my_authors})
