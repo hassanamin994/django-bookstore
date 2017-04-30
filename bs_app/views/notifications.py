5058ad7b22b29e3036fe539e1d9416156da29bd1
Hassan Amin <hassanmamin994@gmail.com>
2017-04-30 07:16:36 +0200

    Notificaitons: Fix: return number of new notifications

--- bs_app/views/notifications.py
+++ bs_app/views/notifications.py
@@ -6,16 +6,12 @@ import json
 # Create your views here.
 
 def set_seen(request):
-
+    request.user.profile.last
     return JsonResponse(request, 'bs_app/home.html',{'categories_books': categories_books, 'authors_books' : authors_books ,'latest_books': latest_books})
 
 def get_notifications(request):
     qs = request.user.profile.notifications.all()[:5]
+    new_notifications = request.user.profile.notifications.filter(id__gt=request.user.profile.last_notification_id).count()
     qs_json = serializers.serialize('json', qs)
-    return JsonResponse({'data':qs_json})
-
-def as_dict(self):
-        return {
-            "id": self.id,
-            # other stuff
-        }
+    print(new_notifications)
+    return JsonResponse({'data':qs_json, 'new_notifications': new_notifications})
